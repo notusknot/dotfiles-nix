16baf4acd641174025025fb3531c5b4ed1af6d6f
notusknot <notusknot@gmail.com>
2021-09-11 22:31:22 -0700

    Fix typo

--- config/hosts/pi.nix
+++ config/hosts/pi.nix
@@ -34,7 +34,7 @@
     boot.initrd.availableKernelModules = [ "usbhid" ];
     fileSystems."/" = { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888"; fsType = "ext4"; };
 
-    swapDevies = [];
+    swapDevices = [];
 
     environment.systemPackages = with pkgs; [ neovim git tmux ];
 
