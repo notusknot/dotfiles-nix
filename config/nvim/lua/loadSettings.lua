544a37d5ee4f195596d79ea2d2fbf0ebaf2d0447
Yabanahano <yabanahano.fui@gmail.com>
2021-11-27 10:03:47 +0700

    Make it organized

--- config/nvim/lua/loadSettings.lua
+++ config/nvim/lua/loadSettings.lua
@@ -5,9 +5,9 @@ local opt = vim.opt
 -- Lazy load everything!
 -- Why Need Absolute Path????
 -- In Lua require/dofile the default dirs is in lua folder
-dofile("galaxyline.lua")
-dofile("lsp.lua")
-dofile("nvim-tree.lua")
+dofile("plugins.galaxyline.lua")
+dofile("plugins.lsp.lua")
+dofile("plugins.nvim-tree.lua")
 
 -- Load All Settings
 settings.load()
